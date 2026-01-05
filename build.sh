4051997d15eae107d3a625e0831e9532cb88fb58
LargeSsun <wkjang74@naver.com>
2026-01-05 05:39:28 +0000

    update image tags for all services to version 2.01

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-docker build -t hyunhojang/auth_server:v2.0 ./auth_server
-docker build -t hyunhojang/employee_server:v2.0 ./employee_server
-docker build -t hyunhojang/gateway:v2.0 ./gateway
-docker build -t hyunhojang/photo_service:v2.0 ./photo_service
-docker build -t hyunhojang/frontend:v2.0 ./frontend
-docker push hyunhojang/auth_server:v2.0
-docker push hyunhojang/employee_server:v2.0
-docker push hyunhojang/gateway:v2.0
-docker push hyunhojang/photo_service:v2.0
-docker push hyunhojang/frontend:v2.0
+docker build -t hyunhojang/auth_server:v2.01 ./auth_server
+docker build -t hyunhojang/employee_server:v2.01 ./employee_server
+docker build -t hyunhojang/gateway:v2.01 ./gateway
+docker build -t hyunhojang/photo_service:v2.01 ./photo_service
+docker build -t hyunhojang/frontend:v2.01 ./frontend
+docker push hyunhojang/auth_server:v2.01
+docker push hyunhojang/employee_server:v2.01
+docker push hyunhojang/gateway:v2.01
+docker push hyunhojang/photo_service:v2.01
+docker push hyunhojang/frontend:v2.01
