b7fb06df5aeae03b5ba1d65c7eb9970238e604e8
LargeSsun <wkjang74@naver.com>
2026-01-08 06:20:29 +0000

    update Dockerfiles to install iproute2 and bump service image tags to version 2.02

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-docker build -t hyunhojang/auth_server:v2.01 ./auth_server
-docker build -t hyunhojang/employee_server:v2.01 ./employee_server
-docker build -t hyunhojang/gateway:v2.01 ./gateway
-docker build -t hyunhojang/photo_service:v2.01 ./photo_service
-docker build -t hyunhojang/frontend:v2.01 ./frontend
-docker push hyunhojang/auth_server:v2.01
-docker push hyunhojang/employee_server:v2.01
-docker push hyunhojang/gateway:v2.01
-docker push hyunhojang/photo_service:v2.01
-docker push hyunhojang/frontend:v2.01
+docker build -t hyunhojang/auth_server:v2.02 ./auth_server
+docker build -t hyunhojang/employee_server:v2.02 ./employee_server
+docker build -t hyunhojang/gateway:v2.02 ./gateway
+docker build -t hyunhojang/photo_service:v2.02 ./photo_service
+docker build -t hyunhojang/frontend:v2.02 ./frontend
+docker push hyunhojang/auth_server:v2.02
+docker push hyunhojang/employee_server:v2.02
+docker push hyunhojang/gateway:v2.02
+docker push hyunhojang/photo_service:v2.02
+docker push hyunhojang/frontend:v2.02
